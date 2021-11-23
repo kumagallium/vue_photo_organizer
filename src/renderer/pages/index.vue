e6a0c789c38968fa7bb6a30b4940ac3a13f0878b
m-kumagai <m-kumagai@sakura.ad.jp>
2021-11-23 20:20:10 +0900

    init

---
+++ src/renderer/pages/index.vue
@@ -0,0 +1,78 @@
+<template>
+    <v-container fluid class="pa-0">
+        <v-row justify="center">
+            <v-col class="pt-8" cols="3" sm="3" md="3">
+              <v-card class="px-md-6 px-sm-6 px-3">
+                <v-img src="file:///Users/m-kumagai/Pictures/VPD.png"/>
+              </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
+      
+</template>
+
+<script>
+import { remote } from 'electron'
+
+export default {
+  components: {
+  },
+  data () {
+    return {
+      externalContent: ''
+    }
+  },
+  methods: {
+    openURL (url) {
+      remote.shell.openExternal(url)
+    }
+  }
+}
+</script>
+
+<style>
+.e-nuxt-container {
+  min-height: calc(100vh - 50px);
+  background: linear-gradient(to right, #ece9e6, #ffffff);
+  font-family: Helvetica, sans-serif;
+}
+
+.e-nuxt-content {
+  display: flex;
+  justify-content: space-around;
+  padding-top: 100px;
+  align-items: flex-start;
+  flex-wrap: wrap;
+}
+
+.e-nuxt-logo{
+  width: 400px;
+}
+
+.e-nuxt-system-info {
+  padding: 20px;
+  border-top: 1px solid #397c6d;
+  border-bottom: 1px solid #397c6d;
+}
+
+.e-nuxt-links {
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+}
+
+.e-nuxt-button {
+  color: #364758;
+  padding: 5px 20px;
+  border: 1px solid #397c6d;
+  margin: 0 20px;
+  border-radius: 15px;
+  font-size: 1rem;
+}
+
+.e-nuxt-button:hover{
+  cursor: pointer;
+  color: white;
+  background-color: #397c6d;
+}
+</style>
